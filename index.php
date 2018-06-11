921d31e9f3cd8be9ef0ea14fb6696ec9dba51b2a
filo-paa <filopaa1990@gmail.com>
2018-06-11 11:23:18 +0200

    server log

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+function getUserIP() {
+    if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+            $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim($addr[0]);
+        } else {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+
+$fh = fopen("server.log", "a");
+
+$useragent = $_SERVER ['HTTP_USER_AGENT'];
+
+fwrite($fh,date("Y-m-d H:i:s") . ", index. IP: " . getUserIP() . ", useragent: ".$useragent.'\n');
+
+ fclose($fh);
+
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
